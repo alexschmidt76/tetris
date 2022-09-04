4220e1c4d37791742040c59c5c7480583ac1f062
Alex Schmidt <alex.schmidt767@gmail.com>
2022-09-04 17:12:16 -0500

    added some functions to detect collisions and clear lines

--- game.js
+++ game.js
@@ -2,10 +2,18 @@ class Game {
     constructor(ctx) {
         this.ctx = ctx;
         this.currentPiece = null;
-        this.grid = Array(20).fill(Array(10).fill(0));
+        this.grid = Array(ROWS).fill(Array(COLS).fill(0));
+    }
+
+    showSelf() {
+        // black background
+        drawRect(this.ctx, 0, 0, COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE, '#000000');
+
+        // show pieces
+        this.grid.forEach( (row, i) => row.forEach( (tile, j) => {
+            if (tile != 0) {
+                drawRect(this.ctx, j * BLOCK_SIZE, i * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE, COLORS[tile]);
+            }
+        }))
     }
 }
-
-let game = new Game('ctx')
-console.log(game.grid)
-console.log(';o;')
